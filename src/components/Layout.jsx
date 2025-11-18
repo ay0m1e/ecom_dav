cf8bf4a6d3a6634cde54dfcf73c4174f62ea9009
Ayomide <ay0m1ealonge@gmail.com>
2025-11-18 15:41:51 +0000

    Added vercel analytics for the website

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function Layout({ children }) {
   return (
@@ -7,6 +9,7 @@ export default function Layout({ children }) {
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
